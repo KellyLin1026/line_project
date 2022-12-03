06cc1551ae9b48f601e0f3ae29ac97a4b7693491
kelly <tlin009@uottawa.ca>
2022-12-03 18:50:56 -0500

    test

--- line-bot-tutorial-master/app.py
+++ line-bot-tutorial-master/app.py
@@ -33,8 +33,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    #message = TextSendMessage(text=event.message.text)
+    #line_bot_api.reply_message(event.reply_token, message)
     if re.match('hello', message):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='good morning'))
     else:
